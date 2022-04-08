e6f0f81d49a9b40d62d2a90af815ee36c14203eb
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-04-08 07:39:01 +0300

    new deploy

---
+++ New.Era.App/App_application/SqlScriptsDebug/drop_all_tables.sql
@@ -0,0 +1,22 @@
+drop table if exists comp.Companies;
+drop table if exists jrn.StockJournal;
+drop table if exists usr.Defaults;
+drop table if exists acc.Accounts;
+drop table if exists doc.DocDetails;
+drop table if exists doc.Documents
+drop table if exists doc.OpJournalStore
+drop table if exists doc.Operations
+drop table if exists doc.DocumentForms
+drop table if exists doc.OperationGroups
+drop table if exists doc.Forms
+drop table if exists doc.FormsMenu
+drop table if exists cat.Agents
+drop table if exists cat.ItemTreeItems
+drop table if exists cat.ItemTree
+drop table if exists cat.Items
+drop table if exists cat.Banks
+drop table if exists cat.Companies
+drop table if exists cat.Warehouses
+drop table if exists cat.Vendors
+drop table if exists cat.Units
+
