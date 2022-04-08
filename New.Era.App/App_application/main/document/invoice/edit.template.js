477f018388936e4638aa0e11c8a5eca8fcfed68f
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-04-08 22:35:27 +0300

    small fixes

--- New.Era.App/App_application/main/document/invoice/edit.template.js
+++ New.Era.App/App_application/main/document/invoice/edit.template.js
@@ -1,9 +1,11 @@
 define(["require", "exports"], function (require, exports) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    const dateUtils = require("std:utils").date;
     const template = {
         defaults: {
-            'Document.Operation'() { return this.Operations[0]; }
+            'Document.Operation'() { return this.Operations[0]; },
+            'Document.Date': dateUtils.today(),
         },
         properties: {},
         commands: {}
--- New.Era.App/App_application/main/document/invoice/edit.template.ts
+++ New.Era.App/App_application/main/document/invoice/edit.template.ts
@@ -1,7 +1,10 @@
 ﻿
+const dateUtils: UtilsDate = require("std:utils").date;
+
 const template: Template = {
 	defaults:{
-		'Document.Operation'(this: any) { return this.Operations[0]; }
+		'Document.Operation'(this: any) { return this.Operations[0]; },
+		'Document.Date': dateUtils.today(),
 	},
 	properties: {
 	},
