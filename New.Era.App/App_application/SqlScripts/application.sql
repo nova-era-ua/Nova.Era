37aa8b27e948bf764d7e67d2cd9885f495752b52
Аlex Kukhtin <alex.kukhtin@outlook.com>
2022-04-11 23:48:26 +0300

    reports

--- New.Era.App/App_application/SqlScripts/application.sql
+++ New.Era.App/App_application/SqlScripts/application.sql
@@ -1,6 +1,6 @@
 ﻿/*
 version: 10.1.1012
-generated: 11.04.2022 20:51:06
+generated: 11.04.2022 22:59:12
 */
 
 
@@ -3181,19 +3181,33 @@ go
 create or alter procedure rep.[Report.Turnover.Item.Load]
 @TenantId int = 1,
 @UserId bigint,
-@Id bigint /* report id */
+@Id bigint, /* report id */
+@From date = null,
+@To date = null
 as
 begin
 	set nocount on;
 	set transaction isolation level read uncommitted;
+
+	set @From = isnull(@From, getdate());
+	set @To = isnull(@To, getdate());
+
 	-- turnover
 	declare @acc bigint;
 	select @acc = Account from rep.Reports where TenantId = @TenantId and Id = @Id;
+
+	select [Data!TData!Array] = null, [Id!!Id] = j.Id, [ItemName] = i.[Name]
+	from jrn.Journal j left join cat.Items i on j.TenantId = i.TenantId and j.Item = i.Id
+		where j.TenantId = @TenantId;
 
 
 	select [Report!TReport!Object] = null, [Name!!Name] = [Name]
 	from rep.Reports where TenantId = @TenantId and Id = @Id;
 	select [Data!TData!Array] = null
+
+	select [!$System!] = null,
+		[!Data.Period.From!TPeriod!Filter] = @From,
+		[!Data.Period.To!TPeriod!Filter] = @To;
 end
 go
 
